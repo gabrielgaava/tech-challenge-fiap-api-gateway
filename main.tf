643bc1965776bde73075759f83c1ac848504c6fe
gaava <nero.gava@gmail.com>
2024-09-17 22:17:15 -0300

    feat: Creating base of API Gateway creation

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    region     = var.aws_region
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    token      = var.aws_session_token
+  }
+  
+  resource "aws_api_gateway_rest_api" "tech_challenge_api" {
+    name        =   "Tech-Challenge-API"
+    description =   "API Gateway para projeto do Tech Challenge na Pos da FIAP"
+    body        =   file("api_contract.yaml")
+  }
+  
+  resource "aws_api_gateway_deployment" "tech_challenge_api_deployment" {
+    
+  depends_on = [
+      aws_api_gateway_rest_api.example_api
+    ]
+    
+    rest_api_id = aws_api_gateway_rest_api.example_api.id
+    stage_name  = var.stage
+  }
+  
+  
+  variable "aws_access_key" {}
+  variable "aws_secret_key" {}
+  variable "aws_session_token" {}
+  variable "aws_region" {}
+  variable "stage" {}
